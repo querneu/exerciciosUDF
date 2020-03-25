e60705671a765394a1e3753eba4d914066f674fe
Lucas Soares <lucasoaresleite@gmail.com>
2020-03-25 05:23:17 -0300

    created function

--- ExercicioPhp/q7.php
+++ ExercicioPhp/q7.php
@@ -3,15 +3,31 @@
     colunas e um conteúdo (texto) qualquer. Que serão utilizados para criar uma tabela
     HTML com essas caracteristicas
 -->
+<?php
+function create_table($linhas, $colunas, $texto){
+    $indexLN = 0;
+    $indexCOL = 0;
+    echo "<table>";
+    while ($indexLN < $linhas) {
+        echo "<tr>";
+        while ($indexCOL < $colunas) {
+            echo "<td>" . $texto . "</td>";
+            $indexCOL++;
+        }
+        echo "</tr>";
+        $indexLN++;
+        $indexCOL = 0;
+    }
+    echo "<table>";
+}
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Questao 7</title>
 </head>
-
 <body>
     <form method="post">
         <input type="number" name="linhas" id="linhas" placeholder="Digite a quantidade de linhas" required>
@@ -20,27 +36,12 @@
         <input type="submit" value="Enviar">
     </form>
     <?php
-        if(isset($_POST['linhas'])&&isset($_POST['colunas'])&&isset($_POST['texto'])){
-            $linhas = $_POST['linhas'];
-            $colunas = $_POST['colunas'];
-            $texto = $_POST['texto'];
-            $indexLN = 0;
-            $indexCOL = 0;
-            echo "<table>";
-            while($indexLN<$linhas){
-                echo "<tr>";
-                    while ($indexCOL < $colunas){
-                        echo "<td>".$texto."</td>";
-                        $indexCOL++;
-                    }
-                echo "</tr>";
-                $indexLN++;
-                $indexCOL=0;
-            }
-            echo "<table>";
-
-        }
+    if (isset($_POST['linhas']) && isset($_POST['colunas']) && isset($_POST['texto'])) {
+        $linhas = $_POST['linhas'];
+        $colunas = $_POST['colunas'];
+        $texto = $_POST['texto'];
+        create_table($linhas,$colunas,$texto);
+    }
     ?>
 </body>
-
 </html>
