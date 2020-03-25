1650abc115d52faaaf209b9654ae5f825eba2433
Lucas Soares <lucasoaresleite@gmail.com>
2020-03-24 22:06:21 -0300

    add description

--- ExercicioPhp/q1.php
+++ ExercicioPhp/q1.php
@@ -1,3 +1,10 @@
+<!--
+    Construir um algoritmo que leia 2 números e efetue a adição. Caso o valor
+    somado seja maior que 20, este deverá ser apresentado somando-se a ele mais 
+    8; caso o valor somado seja menor ou igual a 20, este deverá ser apresentado 
+    subtraindo-se 5. 
+-->
+
 <?php 
 if(isset($_POST['valor1']) && isset($_POST['valor2'])){
         $valor2 = $_POST['valor2'];
